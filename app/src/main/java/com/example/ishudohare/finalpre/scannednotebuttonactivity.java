55b1d6656ccc373f0f59243d762dd3429831cc19
ISHU DOHARE <ishu.dohare.ece15@itbhu.ac.in>
2017-03-16 04:04:21 +0530

    initial commit

---
+++ app/src/main/java/com/example/ishudohare/finalpre/scannednotebuttonactivity.java
@@ -0,0 +1,16 @@
+package com.example.ishudohare.finalpre;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+/**
+ * Created by ISHU DOHARE on 17-01-2017.
+ */
+public class scannednotebuttonactivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        setContentView(R.layout.addnotesfromscannedtext);
+        super.onCreate(savedInstanceState, persistentState);
+    }
+}
